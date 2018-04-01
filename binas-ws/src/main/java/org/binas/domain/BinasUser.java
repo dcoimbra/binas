3c72f255bb15a8b2e00f7bf108a39d43192d1a8a
filipaBit <filipalpgouveia@gmail.com>
2018-04-02 00:55:14 +0100

    create class for Bina Users

---
+++ binas-ws/src/main/java/org/binas/domain/BinasUser.java
@@ -0,0 +1,53 @@
+package org.binas.domain;
+
+public class BinasUser {
+	
+	private final String email;
+	private String password;
+	private int credit;
+	private boolean withBina;
+
+	
+	public BinasUser(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	
+	public void changeCredit(int credit) {
+		this.credit += credit;
+	}
+	
+	//getters and setters
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getCredit() {
+		return credit;
+	}
+	
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	
+	public boolean isWithBina() {
+		return withBina;
+	}
+	
+	public void setWithBina(boolean withBina) {
+		this.withBina = withBina;
+	}
+	
+	
+
+	
+
+}
