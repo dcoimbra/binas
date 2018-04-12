358fcd9127f41248f586e85de56de552eb5739b9
Lucia Lisboa <lucy.hf.c@hotmail.com>
2018-04-12 21:28:49 +0100

    Implement basic testing on GetInfoStation, refers to #5
    test.properties changed too!
    uddi.enabled = true

--- binas-ws-cli/src/test/java/org/binas/ws/it/GetInfoStationIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/GetInfoStationIT.java
@@ -1,4 +1,29 @@
 package org.binas.ws.it;
 
+import org.binas.ws.InvalidStation_Exception;
+import org.junit.AfterClass;
+import org.junit.Test;
+
 public class GetInfoStationIT extends BaseIT {
+
+    @Test(expected = InvalidStation_Exception.class)
+    public void getInfoStationEmptyTest() throws InvalidStation_Exception {
+        client.getInfoStation("");
+    }
+
+
+    @Test(expected = InvalidStation_Exception.class)
+    public void getInfoStationNullTest() throws InvalidStation_Exception {
+        client.getInfoStation(null);
+    }
+
+    @Test(expected = InvalidStation_Exception.class)
+    public void getInfoStationInvalidStationTest() throws InvalidStation_Exception {
+        client.getInfoStation("hell");
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        client.testClear();
+    }
 }
