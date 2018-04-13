0ff160ee59e43e41c2862b93e204d4296bafc953
filipaBit <filipalpgouveia@gmail.com>
2018-04-13 16:56:11 +0100

    changes station names to atribute variable defined in BaseIT

--- binas-ws-cli/src/test/java/org/binas/ws/it/GetInfoStationIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/GetInfoStationIT.java
@@ -12,11 +12,11 @@ public class GetInfoStationIT extends BaseIT {
     @Test
     public void success() throws InvalidStation_Exception, BadInit_Exception {
     									// x, y, capacity, returnPrize
-       client.testInitStation("T07_Station1", 30, 40, 200, 20); 
+       client.testInitStation(STATION_ID1, 30, 40, 200, 20); 
 
-       StationView station = client.getInfoStation("T07_Station1");
+       StationView station = client.getInfoStation(STATION_ID1);
 
-        Assert.assertEquals("T07_Station1", station.getId());
+        Assert.assertEquals(STATION_ID1, station.getId());
         Assert.assertEquals(30, (int) station.getCoordinate().getX());
         Assert.assertEquals(40, (int) station.getCoordinate().getY());
         Assert.assertEquals(200, station.getCapacity());
@@ -25,29 +25,29 @@ public class GetInfoStationIT extends BaseIT {
 
     @Test
     public void getInfoEmptyTest() throws BadInit_Exception, InvalidStation_Exception {
-        client.testInitStation("T07_Station1", 10, 10, 20, 0);
-        Assert.assertNotNull(client.getInfoStation("T07_Station1"));
+        client.testInitStation(STATION_ID1, 10, 10, 20, 0);
+        Assert.assertNotNull(client.getInfoStation(STATION_ID1));
     }
 
 
     @Test(expected = BadInit_Exception.class)
     public void negativeCoord() throws BadInit_Exception {
-        client.testInitStation("T07_Station1",-10, -20, 5, 0);
+        client.testInitStation(STATION_ID1,-10, -20, 5, 0);
     }
 
     @Test(expected = BadInit_Exception.class)
     public void outOfBoundsCoord() throws BadInit_Exception {
-        client.testInitStation("T07_Station1",300, 300, 5, 0);
+        client.testInitStation(STATION_ID1,300, 300, 5, 0);
     }
 
     @Test(expected = BadInit_Exception.class)
     public void zeroCapacity() throws BadInit_Exception {
-        client.testInitStation("T07_Station1",10, 10, 0, 0);
+        client.testInitStation(STATION_ID1,10, 10, 0, 0);
     }
 
     @Test(expected = BadInit_Exception.class)
     public void negativeCapacity() throws BadInit_Exception {
-        client.testInitStation("T07_Station1",-10, -10, 0, 0);
+        client.testInitStation(STATION_ID1,-10, -10, 0, 0);
     }
 
     @Test(expected = InvalidStation_Exception.class)
