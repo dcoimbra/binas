ed5876e4ae4eabfd652fee9703143fd982cdf860
Lucia Lisboa <lucy.hf.c@hotmail.com>
2018-04-13 09:29:00 +0100

    Implement RentBina and ReturnBina testing, refers to #5

--- binas-ws-cli/src/test/java/org/binas/ws/it/ReturnBinaIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/ReturnBinaIT.java
@@ -1,4 +1,35 @@
 package org.binas.ws.it;
 
 public class ReturnBinaIT extends BaseIT{
+
+ /*   @BeforeClass
+    public static void setUp() throws Exception{
+        client.testInit(10);
+        client.rentBina("T07_Station01", "david@tecnico.pt");
+    }
+
+    @Test(expected = InvalidStation_Exception.class)
+    public void returnBinaEmptyStationTest() throws UserNotExists_Exception, NoBinaRented_Exception, FullStation_Exception, InvalidStation_Exception {
+        client.returnBina("", "david@tecnico.pt");
+    }
+
+    @Test(expected = InvalidEmail_Exception.class)
+    public void returnBinaEmptyEmailTest() throws UserNotExists_Exception, NoBinaRented_Exception, FullStation_Exception, InvalidStation_Exception {
+        client.returnBina("T07_Station01", "");
+    }
+
+    @Test(expected = InvalidEmail_Exception.class)
+    public void returnBinaNullEmailTest() throws UserNotExists_Exception, NoBinaRented_Exception, FullStation_Exception, InvalidStation_Exception {
+        client.returnBina("T07_Station01", null);
+    }
+
+    @Test(expected = InvalidStation_Exception.class)
+    public void returnBinaNullStationTest() throws UserNotExists_Exception, NoBinaRented_Exception, FullStation_Exception, InvalidStation_Exception{
+        client.returnBina(null, "lucia@not.com");
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        client.testClear();
+    }*/
 }
