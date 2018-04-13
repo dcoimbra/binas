5bc7a433fe0a1292b82b8d49ff7caf3f561cf58f
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2018-04-13 02:11:43 +0100

    Binas tests

--- binas-ws/src/main/java/org/binas/domain/BinasManager.java
+++ binas-ws/src/main/java/org/binas/domain/BinasManager.java
@@ -190,10 +190,16 @@ public class BinasManager {
 
 	public void reset() {
 
+		BinasUser.getEmails().clear();
 		users.clear();
 	}
 
 	public void testInitStation(String stationId, int x, int y, int capacity, int returnPrize) throws BadInitException {
+
+		if(x < 0 || y < 0 || capacity <= 0 || returnPrize < 0)
+			throw new BadInitException();
+		if(x > 100 || y > 100)
+			throw new BadInitException();
 
 		StationClient client = stationClients.get(stationId);
 
@@ -210,11 +216,18 @@ public class BinasManager {
 		stationViews.put(newStationView.getId(), newStationView);
 	}
 
-	public void testInit() throws BadInitException {
+	public void testInit(int userInitialPoints) throws BadInitException {
+
+		if (userInitialPoints < 0 ) {
+
+			throw new BadInitException("Credit must be non negative");
+		}
 
 		BinasUser user = null;
 		try {
 			user = activateUser("david@tecnico.pt");
+
+			user.setCredit(userInitialPoints);
 		} catch (EmailExistsException e) {
 			throw new BadInitException(e.getMessage());
 		} catch (InvalidEmailException e) {
--- binas-ws/src/main/java/org/binas/ws/BinasPortImpl.java
+++ binas-ws/src/main/java/org/binas/ws/BinasPortImpl.java
@@ -137,7 +137,7 @@ public class BinasPortImpl implements BinasPortType {
     	return result;
     }
 
-	/** Delete all users and stations. */
+	/** Delete all users. */
     @Override
     public void testClear(){
 		BinasManager.getInstance().reset();
@@ -178,7 +178,7 @@ public class BinasPortImpl implements BinasPortType {
     public void testInit(int userInitialPoints) throws BadInit_Exception {
 
 		try {
-			BinasManager.getInstance().testInit();
+			BinasManager.getInstance().testInit(userInitialPoints);
 		}
 
 		catch (BadInitException e) {
